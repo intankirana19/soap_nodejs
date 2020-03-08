eb1687db5c5e9cf78a9d27f4a9374af626a8863b
Budi Kusuma Utama <budi@Budis-MacBook-Pro.local>
2020-03-08 19:51:36 +0700

    send sms method V2

--- modules/vendor/sms.js
+++ modules/vendor/sms.js
@@ -3,59 +3,30 @@ var auth = require('../shared/auth');
 
 function sendSMS(req,res){
   let user64 = auth.smsUser();
-  console.log(user64)
-  console.log(global.gConfig.api_reg);
-//   var options = {
-//     method: 'POST',
-//     uri: global.gConfig.api_reg+'sendsms',
-//     headers: {
-//       'Authorization': 'Basic '+user64
-//     },
-//     body: {
-//       sender: 'Sanbox user',
-//       msisdn: '081973290873',
-//       message: 'test sms fkldafdahflkdhalfhda'
-//     },
-//     json: true
-//   };
-//    console.log(options)
-//   request(options)
-//   .then(function (response) {
-//     console.log(response);
-//   });
-// }
-// console.log('eXB0cmlhbDpEQURJMTVkTA==')
-var formData = {
-  // Pass a simple key-value pair
-  username: 'yptrial',
-  password: 'DADI15dL',
-  sender : 'test',
-  message: 'test sms',
-  msisdn: '081973290873',
 
-};
-request.post({url:'https://api.smsblast.id/v2/reg/sendsms',headers: {'Authorization': 'Basic '+ user64}, formData: formData}, function optionalCallback(err, httpResponse, body) {
-  if (err) {
-    return console.error('send failed:', err);
-  }
-  console.log('Sent successful!  Server responded with:', body);
-});
+  var formData = {
+    sender : 'MD Media',
+    message: 'test sms',
+    msisdn: '081973290873',
+
+  };
+  request.post({url: global.gConfig.api_reg+'sendsms.json',headers: {'Authorization': 'Basic '+ user64}, form: formData}, function optionalCallback(err, httpResponse, body) {
+    if (err) {
+      res.status(400)
+            .json({
+                status: 'error',
+                message: err
+            });
+    }else{
+      res.status(200)
+      .json({
+          status: 'success',
+          message: JSON.parse(body)
+      });
+    }
+  });
 }
 
-// var formData = {
-//   // Pass a simple key-value pair
-//   username: 'getplus-su',
-//   password: 'getplusBwx',
-//   message: message,
-//   msisdn: '081973290873',
-
-// };
-// request.post({url:'http://api.mitrabiz.id:8080/mitraBizSmsV3/api/send_single', formData: formData}, function optionalCallback(err, httpResponse, body) {
-//   if (err) {
-//     return console.error('send failed:', err);
-//   }
-//   console.log('Sent successful!  Server responded with:', body);
-// });
 
 module.exports = {
   sendSMS:sendSMS
