5df257b77929d314abef4f8bf268027131cf0b42
Intan Kirana <intan.kirana@brainworx.co.id>
2020-04-13 11:19:30 +0700

    add new routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+// const cron = require('node-cron');
 
 //modules
 var index = require('../modules/index');
@@ -7,8 +8,9 @@ var upload = require('../modules/vendor/upload');
 var auth = require('../modules/shared/auth');
 var sms = require('../modules/vendor/sms');
 var dsms = require('../modules/dashboard/sms/message');
-var account = require('../modules/shared/account');
-var client = require('../modules/shared/client');
+var account = require('../modules/dashboard/account');
+var client = require('../modules/dashboard/client');
+var tokenReset = require('../modules/dashboard/token');
 var smsReport = require('../modules/dashboard/sms/reports');
 var otpReport = require('../modules/dashboard/otp/reports');
 var smsToken = require('../modules/dashboard/sms/token');
@@ -29,23 +31,28 @@ router.post('/sms/create', dsms.createSms)
 //send SMS
 router.post('/sms', sms.sendSMS)
 
-//Account
+//Dashboard-Account
 router.get('/account/list', account.getAllAccounts);
 router.post('/account/create', account.createAccount);
 router.patch('/account/edit', account.editAccount);
 router.patch('/account/activate', account.activateAccount);
 router.patch('/account/delete', account.deleteAccount);
 
-//Account Roles
+//Dashboard-Account Roles
 router.get('/account/roleslist', account.getRoles);
 router.get('/account/role', account.accountRole);
 
-//Clients
+//Dashboard-Clients
 router.get('/client/list', client.getAllClients);
 router.post('/client/add', client.addClient);
 router.patch('/client/edit', client.editClient);
 router.patch('/client/activate', client.activateClient);
 router.patch('/client/delete', client.deleteClient);
+
+// Dashboard- Monthly Token Reset Amount
+router.get('/reset/list', tokenReset.getList);
+router.post('/reset/add', tokenReset.add);
+router.patch('/reset/edit', tokenReset.edit);
 
 //Dashboard-sms
 router.get('/sms/getreports', smsReport.getSmsReport);
@@ -58,6 +65,7 @@ router.get('/otp/getreports', otpReport.getOtpReport);
 router.get('/otp/gettokenlist', otpToken.getAllClientOtpToken);
 router.get('/otp/getclienttoken', otpToken.getClientOtpToken);
 router.post('/otp/topupclienttoken', otpToken.topUpClientOtpToken);
+
 
 
 // router.post('/report/save', report.saveReport);
